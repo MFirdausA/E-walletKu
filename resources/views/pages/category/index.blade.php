2229e2564e7d4b112be37167db997d9fc7d0d6f0
MFirdausA <mfirdausakbar88@gmail.com>
2025-02-24 21:06:52 +0700

    update: get data category

--- resources/views/pages/category/index.blade.php
+++ resources/views/pages/category/index.blade.php
@@ -1,24 +1,21 @@
 @extends('layouts.app')
 <title>Category</title>
 @section('content')
-<div class="container p-6 bg-white">
+<div class="container p-6">
 
-    <div class="font-bold flex justify-center items-center bg-white text-xl mb-6">Add Your Category</div>
+    <div class="font-bold flex justify-center items-center text-xl mb-6">Add Your Category</div>
 
-    <div class="min-h-screen flex flex-col justify-center items-start bg-white">
+    <div class="flex flex-col justify-center items-start">
         <div class=" grid-flow-col auto-cols-3">
             <div class="flex justify-start items-center">
-                <button class="flex items-center bg-[#ffa500] text-white font-bold py-2 px-4 rounded-lg gap-2">
+                <button class="flex items-center bg-[#ffa500] text-white font-bold mb-2 py-2 px-4 rounded-lg gap-2">
                     <span>Add Category</span>
                     <span class="bg-white text-[#ffa500] rounded-full w-6 h-6 flex items-center justify-center">+</span>
                 </button>
             </div>
-            <div class="grid-flow-col auto-cols-3">
-                <button class="border border-[#ffa500] py-2 px-4 my-1 mx-0.2 rounded-lg">category 1</button>
-                <button class="border border-[#ffa500] py-2 px-4 my-1 mx-0.2 rounded-lg">category 2</button>
-                <button class="border border-[#ffa500] py-2 px-4 my-1 mx-0.2 rounded-lg">category 3</button>
-                <button class="border border-[#ffa500] py-2 px-4 my-1 mx-0.2 rounded-lg">category 4</button>
-            </div>
+            @foreach ( $categories as $category )
+            <button class="grid-flow-col auto-cols-3 border border-[#ffa500] py-2 px-4 my-1 mx-0.2 rounded-lg">{{ $category->name}}</button>
+            @endforeach
         </div>
     </div>
 </div>
