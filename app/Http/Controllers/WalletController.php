21aff7dfdb320f558286f8733965ffba3b337c35
MFirdausA <mfirdausakbar88@gmail.com>
2025-02-24 21:07:27 +0700

    update: get data by user id

--- app/Http/Controllers/WalletController.php
+++ app/Http/Controllers/WalletController.php
@@ -2,11 +2,12 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\wallet;
+use App\Models\transfer;
 use App\Models\transaction;
 use Illuminate\Http\Request;
 use App\Models\TransactionType;
-use App\Models\transfer;
-use App\Models\wallet;
+use Illuminate\Support\Facades\Auth;
 
 class WalletController extends Controller
 {
@@ -23,23 +24,28 @@ class WalletController extends Controller
      */
     public function create()
     {
-        $transaction = transaction::all();
-        $wallets = Wallet::all()->map(function ($wallet) {
+        $user = Auth::user()->id;
+        $transaction = transaction::where('user_id', $user)->get();
+        $wallets = Wallet::whereNull('user_id')->orWhere('user_id', $user)->get()->map(function ($wallet) use ($user) {
             $incomeAmount = Transaction::where('wallet_id', $wallet->id)
                 ->whereHas('transactionType', function ($query) {
                     $query->where('name', 'Income');
                 })
+                ->where('user_id', $user)
                 ->sum('amount');
     
             $expenseAmount = Transaction::where('wallet_id', $wallet->id)
                 ->whereHas('transactionType', function ($query) {
                     $query->where('name', 'Expense');
                 })
+                ->where('user_id', $user)
                 ->sum('amount');
-            
+
             $tranferIn = Transfer::where('to_wallet_id', $wallet->id)
+                ->where('user_id', $user)
                 ->sum('amount');
             $tranferOut = Transfer::where('wallet_id', $wallet->id)
+                ->where('user_id', $user)
                 ->sum('amount');
                 
             $wallet->incomeAmount = $incomeAmount + $tranferIn;
