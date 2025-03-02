1d456f8023c9cc99d9cf4ea17f991b50af33dcc7
MFirdausA <mfirdausakbar88@gmail.com>
2025-03-02 10:08:19 +0700

    update: page create(manage category)

--- resources/views/pages/category/create.blade.php
+++ resources/views/pages/category/create.blade.php
@@ -2,43 +2,76 @@
 <title>Category</title>
 @section('content')
 <link href="https://cdn.jsdelivr.net/npm/flowbite@3.1.2/dist/flowbite.min.css" rel="stylesheet" />
-
 <div class="container p-6">
 
     <div class="flex justify-start items-center w-full mb-4">
-            <a href="{{ route('category.index') }}" >
-                <img class="w-4" src="{{ asset('img/back.svg') }}" alt="">
-            </a>
+        <a href="{{ route('category.index') }}" >
+            <img class="w-4" src="{{ asset('img/back.svg') }}" alt="">
+        </a>
         <div class="flex items-center justify-center w-full">
-            <h1 class="text-2xl font-bold">Categories</h1>
+            <h1 class="text-2xl font-bold">Manage Category</h1>
         </div>
     </div>
-    <div class="flex flex-col justify-center items-start w-full">
-        <div class="w-full">
-            <div class="flex flex-col justify-center items-start">
-                <div class=" grid-flow-col auto-cols-4">
-                    <div class="flex justify-start items-center">
-                        <button data-modal-target="addWalletModal" data-modal-toggle="addWalletModal" type="button" class="flex items-center bg-[#ffa500] text-white font-bold mb-2 py-2 px-4 rounded-lg gap-2">
-                            <span>Add Category</span>
-                            <span class="bg-white text-[#ffa500] rounded-full w-6 h-6 flex items-center justify-center">+</span>
-                        </button>
+
+    <div class="flex flex-col justify-center items-start">
+        <div class=" grid-flow-col auto-cols-4">
+            <div class="flex justify-start items-center">
+                <button data-modal-target="addWalletModal" data-modal-toggle="addWalletModal" type="button" class="flex items-center bg-[#ffa500] text-white font-bold py-2 px-4 rounded-lg gap-2">
+                    <span>Add category</span>
+                    <span class="bg-white text-[#ffa500] rounded-full w-6 h-6 flex items-center justify-center">+</span>
+                </button>
+            </div>
+            <div class="grid-flow-col auto-cols-4">
+                @foreach ($categories as $category)
+                <button data-modal-target="editWalletModal{{ $category->id }}" data-modal-toggle="editWalletModal{{ $category->id }}" type="button" class="border border-[#ffa500] py-2 px-4 my-1 mx-0.2 rounded-lg">
+                    <div class="flex items-center gap-1">
+                        <img src="{{ asset('storage/'.$category->cover) }}" class="w-6" alt="img"><span>{{ $category->name }}</span>
                     </div>
-                    @foreach ( $categories as $category )
-                    <button class="grid-flow-col auto-cols-4 border border-[#ffa500] py-2 px-4 my-1 mx-0.2 rounded-lg flex">
-                        <div class="flex justify-start items-center gap-1">
-                            <img class="w-6" src="{{ asset('storage/' . $category->cover) }}" alt="">
-                            {{ $category->name}}
+                </button>
+                <div id="editWalletModal{{ $category->id }}" tabindex="-1" aria-hidden="true" class="hidden overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 justify-center items-center w-full md:inset-0 h-[calc(100%-1rem)] max-h-full">
+                    <div class="relative p-4 w-full max-w-md max-h-full">
+                        <!-- Modal content -->
+                        <div class="relative bg-white rounded-lg shadow-sm">
+                            <!-- Modal header -->
+                            <div class="flex items-center justify-between p-4 md:p-5 border-b rounded-t border-gray-200">
+                                <h3 class="text-lg font-semibold text-black">
+                                    Update category
+                                </h3>
+                                <button type="button" class="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ms-auto inline-flex justify-center items-center dark:hover:bg-gray-600 dark:hover:text-white" data-modal-toggle="editWalletModal{{ $category->id }}">
+                                    <svg class="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 14">
+                                        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6"/>
+                                    </svg>
+                                    <span class="sr-only">Close modal</span>
+                                </button>
+                            </div>
+                            <!-- Modal body -->
+                            <form action="{{ route('category.update', $category->id) }}" method="POST" enctype="multipart/form-data" class="p-4 md:p-5">
+                                @csrf
+                                @method('PUT')
+                                <div class="flex flex-col gap-4 mb-4">
+                                    <div>
+                                        <x-input-label for="name" :value="__('Name')" />
+                                        <x-text-input id="name" name="name" type="text" class="mt-1 block w-full" value="{{ $category->name ?? old('name') }}"  autofocus autocomplete="name" />
+                                        <x-input-error class="mt-2" :messages="$errors->get('name')" />
+                                    </div>
+                                    <div class="flex flex-col">
+                                        <x-input-label for="cover" :value="__('Icon')" />
+                                        @if ($category->cover)
+                                        <img src="{{ asset('storage/'.$category->cover) }}" class="w-10" alt="img">
+                                        <span>{{ $category->cover }}</span>
+                                        @endif
+                                        <input type="file" id="fileInput" name="cover" value="{{ old('cover') }}" class="mb-2 w-full text-sm text-gray-500 file:py-2 rounded-full  file:text-sm file:font-semibold file:bg-[#ffa500] file:text-white hover:file:bg-[#cc8400]">
+                                        <x-input-error class="mt-2" :messages="$errors->get('cover')" />
+                                    </div>
+                                </div>
+                                <x-primary-button>
+                                    {{ __('Save') }}
+                                </x-primary-button>
+                            </form>
                         </div>
-                    </button>
-                    <form action="{{ route('income.destroy', $category->id) }}" method="POST">
-                        @csrf
-                        @method('DELETE')
-                        <button class="bg-white p-2 rounded-lg shadow-lg flex items-center">
-                            <img src="{{ asset('img/trash-icon.svg') }}" alt="">
-                        </button>
-                    </form>
-                    @endforeach
+                    </div>
                 </div>
+                @endforeach
             </div>
         </div>
     </div>
@@ -51,7 +84,7 @@
                     <h3 class="text-lg font-semibold text-black">
                         Add new category
                     </h3>
-                    <button type="button" class="text-gray-400 bg-transparent hover:bg-red-500 hover:text-white rounded-lg text-sm w-8 h-8 ms-auto inline-flex justify-center items-center" data-modal-toggle="addWalletModal">
+                    <button type="button" class="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ms-auto inline-flex justify-center items-center dark:hover:bg-gray-600 dark:hover:text-white" data-modal-toggle="addWalletModal">
                         <svg class="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 14">
                             <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6"/>
                         </svg>
@@ -80,7 +113,8 @@
                 </form>
             </div>
         </div>
-    </div> 
+    </div>
+    
 </div>
 <script src="https://cdn.jsdelivr.net/npm/flowbite@3.1.2/dist/flowbite.min.js"></script>
 @endsection
