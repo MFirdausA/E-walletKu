20c64be168305f5fb952096505e208837c1c4ba1
MFirdausA <mfirdausakbar88@gmail.com>
2025-04-15 10:45:03 +0700

    update: add logout in profile

--- resources/views/profile/edit.blade.php
+++ resources/views/profile/edit.blade.php
@@ -28,6 +28,21 @@
             </div>
         </div>
 
+        <div class="p-4 w-full bg-white shadow sm:rounded-lg">
+            <h1 class="text-lg font-medium text-gray-900">Logout Account</h1>
+
+            <p class="mt-1 text-sm text-gray-400">
+                {{ __('securely logs the user out of account.') }}
+            </p>
+
+            <form method="POST" action="{{ route('logout')}}" class="w-full flex">
+                @csrf
+                <button type="submit" class="w-full items-center px-4 py-2 mt-3 bg-red-600 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-red-500 active:bg-red-700 focus:outline-none focus:ring-2 focus:ring-red-500 focus:ring-offset-2 dark:focus:ring-offset-gray-800 transition ease-in-out duration-150">
+                    Logout
+                </button>
+            </form>
+        </div>
+
         <div class="p-4 sm:p-8 bg-white shadow sm:rounded-lg">
             <div class="max-w-xl">
                 @include('profile.partials.delete-user-form')
