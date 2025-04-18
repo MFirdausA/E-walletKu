367f7bef5a7c5fa58dd553acd06623ef494ad9d3
MFirdausA <mfirdausakbar88@gmail.com>
2025-04-18 16:51:32 +0700

    update: sensitive case name model

--- app/Livewire/StatsDashboardWidget.php
+++ app/Livewire/StatsDashboardWidget.php
@@ -4,7 +4,7 @@ namespace App\Livewire;
 
 use App\Models\Category;
 use App\Models\User;
-use App\Models\wallet;
+use App\Models\Walletallet;
 use Filament\Widgets\StatsOverviewWidget as BaseWidget;
 use Filament\Widgets\StatsOverviewWidget\Stat;
 
@@ -14,8 +14,8 @@ class StatsDashboardWidget extends BaseWidget
     {
         return [
             Stat::make('Total Users', User::where('id', '<>', 1)->count()),
-            Stat::make('Total Wallet', wallet::count()),
-            Stat::make('Total Category', category::count()),
+            Stat::make('Total Wallet', Wallet::count()),
+            Stat::make('Total Category', Category::count()),
         ];
     }
 }
